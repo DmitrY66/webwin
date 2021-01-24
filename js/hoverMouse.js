6fe162fcf0215acac771cfdaafebe3ac5b9d0d6a
Dmitri <ss808@mail.ru>
2021-01-24 17:45:18 +0500

    changed to cycle
    changed to cycle

--- js/hoverMouse.js
+++ js/hoverMouse.js
@@ -1,85 +1,42 @@
-const section_3_img_filter1 = document.querySelector(".section-3-img-filter1");
-const section_3_img_filter2 = document.querySelector(".section-3-img-filter2");
-const section_3_img_filter3 = document.querySelector(".section-3-img-filter3");
-const section_3_img_filter4 = document.querySelector(".section-3-img-filter4");
-const section_3_img_filter5 = document.querySelector(".section-3-img-filter5");
-const section_3_img_filter6 = document.querySelector(".section-3-img-filter6");
-const img_filter_p1 = document.querySelector(".img-filter-p1");
-const img_filter_p2 = document.querySelector(".img-filter-p2");
-const img_filter_p3 = document.querySelector(".img-filter-p3");
-const img_filter_p4 = document.querySelector(".img-filter-p4");
-const img_filter_p5 = document.querySelector(".img-filter-p5");
-const img_filter_p6 = document.querySelector(".img-filter-p6");
-const img_filter1 = document.querySelector(".img-filter1");
-const img_filter2 = document.querySelector(".img-filter2");
-const img_filter3 = document.querySelector(".img-filter3");
-const img_filter4 = document.querySelector(".img-filter4");
-const img_filter5 = document.querySelector(".img-filter5");
-const img_filter6 = document.querySelector(".img-filter6");
-// 1 ===================================================================
-section_3_img_filter1.addEventListener("mouseover", function (e) {
-  img_filter_p1.classList.add("fadeInUp");
-  img_filter_p1.style.opacity = 1;
-  img_filter1.classList.add("img-filter-hover");
-});
-section_3_img_filter1.addEventListener("mouseout", function (e) {
-  img_filter_p1.classList.remove("fadeInUp");
-  img_filter_p1.style.opacity = 0;
-  img_filter1.classList.remove("img-filter-hover");
-});
-// 2 ===================================================================
-section_3_img_filter2.addEventListener("mouseover", function (e) {
-  img_filter_p2.classList.add("fadeInUp");
-  img_filter_p2.style.opacity = 1;
-  img_filter2.classList.add("img-filter-hover");
-});
-section_3_img_filter2.addEventListener("mouseout", function (e) {
-  img_filter_p2.classList.remove("fadeInUp");
-  img_filter_p2.style.opacity = 0;
-  img_filter2.classList.remove("img-filter-hover");
-});
-// 3 ===================================================================
-section_3_img_filter3.addEventListener("mouseover", function (e) {
-  img_filter_p3.classList.add("fadeInUp");
-  img_filter_p3.style.opacity = 1;
-  img_filter3.classList.add("img-filter-hover");
-});
-section_3_img_filter3.addEventListener("mouseout", function (e) {
-  img_filter_p3.classList.remove("fadeInUp");
-  img_filter_p3.style.opacity = 0;
-  img_filter3.classList.remove("img-filter-hover");
-});
-// 4 ===================================================================
-section_3_img_filter4.addEventListener("mouseover", function (e) {
-  img_filter_p4.classList.add("fadeInUp");
-  img_filter_p4.style.opacity = 1;
-  img_filter4.classList.add("img-filter-hover");
-});
-section_3_img_filter4.addEventListener("mouseout", function (e) {
-  img_filter_p4.classList.remove("fadeInUp");
-  img_filter_p4.style.opacity = 0;
-  img_filter4.classList.remove("img-filter-hover");
-});
-// 5 ===================================================================
-section_3_img_filter5.addEventListener("mouseover", function (e) {
-  img_filter_p5.classList.add("fadeInUp");
-  img_filter_p5.style.opacity = 1;
-  img_filter5.classList.add("img-filter-hover");
-});
-section_3_img_filter5.addEventListener("mouseout", function (e) {
-  img_filter_p5.classList.remove("fadeInUp");
-  img_filter_p5.style.opacity = 0;
-  img_filter5.classList.remove("img-filter-hover");
-});
-// 6 ===================================================================
-section_3_img_filter6.addEventListener("mouseover", function (e) {
-  img_filter_p6.classList.add("fadeInUp");
-  img_filter_p6.style.opacity = 1;
-  img_filter6.classList.add("img-filter-hover");
-});
-section_3_img_filter6.addEventListener("mouseout", function (e) {
-  img_filter_p6.classList.remove("fadeInUp");
-  img_filter_p6.style.opacity = 0;
-  img_filter6.classList.remove("img-filter-hover");
-});
-// ===================================================================
+let section_3_img_filter = document.querySelectorAll(".section-3-img-filter");
+let img_filter_p = document.querySelectorAll(".img-filter-p");
+let img_filter = document.querySelectorAll(".img-filter");
+
+
+for (let i = 0; i < section_3_img_filter.length; i++) {
+
+  section_3_img_filter[i].addEventListener("mouseenter", function (e) {
+
+    for (let j = 0; j < img_filter_p.length; j++) {
+      if (i == j) {
+        img_filter_p[j].classList.add("fadeInUp");
+        img_filter_p[j].style.opacity = 1;
+      }
+    }
+
+    for (let k = 0; k < img_filter.length; k++) {
+      if (i == k) {
+        img_filter[k].classList.add("img-filter-hover");
+      }
+    }
+
+  })
+
+  section_3_img_filter[i].addEventListener("mouseleave", function (e) {
+
+    for (let j = 0; j < img_filter_p.length; j++) {
+      if (i == j) {
+        img_filter_p[j].classList.remove("fadeInUp");
+        img_filter_p[j].style.opacity = 0;
+      }
+    }
+
+    for (let k = 0; k < img_filter.length; k++) {
+      if (i == k) {
+        img_filter[k].classList.remove("img-filter-hover");
+      }
+    }
+
+  })
+
+}
